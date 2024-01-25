3a93bc998f50d575342d391d4275f2506ae8db1f
Nitesh Kumar <niteshkumar240198@gmail.com>
2024-01-25 23:12:28 +0530

    update and delete controller added

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -48,3 +48,164 @@ const addNewUser = async (req, res) => {
         return res.status(500).send(databaseErrorResponse(error.message));
     }
 }
+
+
+
+/**
+ * Retrieves user information based on the user ID provided in the request parameters or All users.
+ *
+ * @param {Object} req - the request object
+ * @param {Object} res - the response object
+ * @return {Promise} a Promise that resolves to the user information or an error response
+ */
+
+const getUsersInfo = async (req, res) => {
+
+   
+}
+
+
+
+
+/**
+ * Updates user information based on the provided user ID.
+ *
+ * @param {Object} req - the request object
+ * @param {Object} res - the response object
+ * @return {Promise} a Promise that resolves with the updated user data
+ */
+
+const updateUserInfo = async (req, res) => {
+
+    const { userId } = req.params;
+
+    if (!userId) {
+        return res.status(400).send(userIdNotProvidedResponse());
+    }
+
+    try {
+
+        const isPresent = await checkUserByUserId(userId);
+        if (!isPresent) {
+            return res.status(404).send(userNotFoundResponse());
+        }
+
+        await UserModel.findOneAndUpdate({ userId }, req.body);
+
+        const user = await checkUserByUserId(userId);
+
+        return res.status(200).send({
+            "message": "User updated successfully",
+            "status": 200,
+            "data": user
+        })
+
+    } catch (error) {
+        return res.status(500).send(databaseErrorResponse(error.message));
+    }
+}
+
+
+/**
+ * Asynchronously removes a user based on the provided userId parameter.
+ *
+ * @param {Object} req - the request object
+ * @param {Object} res - the response object
+ * @return {Object} the response object with the result of the user removal operation
+ */
+
+const removeUser = async (req, res) => {
+
+    const { userId } = req.params;
+
+    if (!userId) {
+        return res.status(400).send(userIdNotProvidedResponse());
+    }
+
+    try {
+
+        const isPresent = await checkUserByUserId(userId);
+        if (!isPresent) {
+            return res.status(404).send(userNotFoundResponse());
+        }
+
+        const user = await UserModel.findOneAndDelete({ userId });
+
+        return res.status(200).send({
+            "message": "User deleted successfully",
+            "status": 200,
+            "data": user
+        })
+
+    } catch (error) {
+        return res.status(500).send(databaseErrorResponse(error.message));
+    }
+}
+
+
+/**
+ * Asynchronously checks the user by user ID.
+ *
+ * @param {String} userId - The user ID to check.
+ * @return {Object} The user found by the user ID.
+ */
+
+async function checkUserByUserId(userId) {
+
+    const user = await UserModel.findOne({ userId });
+    return user;
+}
+
+
+/**
+ * Function to generate a response when user is not found.
+ *
+ * @return {object} Response object with message, status, and data
+ */
+function userNotFoundResponse() {
+
+    return {
+        "message": "User not found",
+        "status": 404,
+        "data": null
+    }
+}
+
+
+/**
+ * Returns a response object with a message indicating that a valid userId was not provided.
+ *
+ * @return {object} The response object with message, status, and data properties.
+ */
+function userIdNotProvidedResponse() {
+
+    return {
+        "message": "Kindly provide a valid userId",
+        "status": 400,
+        "data": null
+    }
+}
+
+
+/**
+ * Creates a database error response object.
+ *
+ * @param {string} message - The error message.
+ * @return {object} The database error response object.
+ */
+function databaseErrorResponse(message) {
+
+    return {
+        "message": message,
+        "status": 500,
+        "data": null
+    }
+}
+
+
+module.exports = {
+    addNewUser,
+    getUsersInfo,
+    updateUserInfo,
+    removeUser
+}
