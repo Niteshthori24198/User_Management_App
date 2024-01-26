a5dbec1652d716a84d00340857e8a45a711a1a4c
Nitesh Kumar <niteshkumar240198@gmail.com>
2024-01-26 11:50:10 +0530

    front end react app setup and nav bar and router added

---
+++ frontend/user-management-app/src/App.js
@@ -0,0 +1,13 @@
+import AllRouters from './components/AllRouters';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRouters />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/user-management-app/src/components/AllRouters.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import AddUser from '../pages/AddUser'
+import Home from '../pages/Home'
+import User from '../pages/User'
+
+function AllRouters() {
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<h2>Welcome to Dashboard</h2>} />
+                <Route path='/Home' element={<Home />} />
+                <Route path='/user' element={<User />} />
+                <Route path='/add-user' element={<AddUser />} />
+                <Route path='/edit-user/:userId' element={<AddUser />} />
+            </Routes>
+        </>
+    )
+}
+
+export default AllRouters
